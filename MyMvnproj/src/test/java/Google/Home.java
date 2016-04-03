fdf8f2b8a2be3da5690fd90032c689717b31194a
venkatgn <venkatadrin.g@gmail.com>
2016-04-03 10:14:37 +0530

    first time

---
+++ MyMvnproj/src/test/java/Google/Home.java
@@ -0,0 +1,54 @@
+package Google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+
+import atu.testng.reports.ATUReports;
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+import atu.testng.reports.logging.LogAs;
+import atu.testng.selenium.reports.CaptureScreen;
+import atu.testng.selenium.reports.CaptureScreen.ScreenshotOf;
+
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class,MethodListener.class})
+public class Home 
+{
+	WebDriver driver;
+	{
+		System.setProperty("atu.reporter.config", "e:/7am_mar_16/myproj/atu.properties");
+	}
+    public Home(WebDriver driver)
+    {
+    	this.driver=driver;
+    }
+	public void open()
+	{
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");		
+	}
+	public void search() throws Exception
+	{
+		driver.findElement(By.id("lst-ib")).sendKeys("selenium");
+		driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+		//driver.findElement(By.xpath("//input[starts-with(@name,'btnG') or starts-with(@name,'btnK')]")).click();	
+		Thread.sleep(5000);
+		try
+		{
+			if(driver.findElement(By.id("resultStats")).isDisplayed())
+			{
+				String res=driver.findElement(By.id("resultStats")).getText();
+				ATUReports.add(res,LogAs.PASSED,new CaptureScreen(ScreenshotOf.DESKTOP));			
+			}
+		}
+		catch(Exception e)
+		{
+			ATUReports.add("Results not displayed",LogAs.FAILED,new CaptureScreen(ScreenshotOf.DESKTOP));
+		}
+		
+	
+	}
+}
---
+++ MyMvnproj/src/test/java/Google/Links.java
@@ -0,0 +1,44 @@
+package Google;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+
+import atu.testng.reports.ATUReports;
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class,MethodListener.class})
+public class Links 
+{
+
+	WebDriver driver;
+	
+	{
+		System.setProperty("atu.reporter.config", "e:/7am_mar_16/myproj/atu.properties");
+	}
+    public Links(WebDriver driver)
+    {
+    	this.driver=driver;
+    }
+	public void getlinks()
+	{
+		List<WebElement> lst=driver.findElements(By.xpath("//a[starts-with(text(),'selenium') or starts-with(text(),'Selenium')]"));
+						
+		for(WebElement x : lst)
+		{
+			ATUReports.add(x.getText(),true);
+		}
+		
+		lst=driver.findElements(By.xpath("//a[contains(text(),'Python')]"));
+		for(WebElement x : lst)
+		{
+			ATUReports.add(x.getText(),true);
+		}
+	}
+}
